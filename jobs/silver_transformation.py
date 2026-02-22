94984f9af9e9ae4ab70537446a3dc7be4ab8a91d
Md Shahedur Rahman <mr6139@nyu.edu>
2026-02-21 21:26:00 -0500

    Add duplicate removal logic

--- jobs/silver_transformation.py
+++ jobs/silver_transformation.py
@@ -4,5 +4,14 @@ from utils.helpers import ensure_dir
 
 
 def run_silver_job(spark):
+    print("Running Silver Transformation...")
+
+    ensure_dir(SILVER_PATH)
+
+    df = spark.read.parquet(BRONZE_PATH)
+
+    transformed = (
+        df.dropDuplicates(["trip_id"])
+    )
 
     return transformed
