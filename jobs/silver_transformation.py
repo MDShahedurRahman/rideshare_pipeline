5320279c363b030b29574ab503dfa43b8baa4301
Md Shahedur Rahman <mr6139@nyu.edu>
2026-02-21 21:25:05 -0500

    Create silver transformation job

---
+++ jobs/silver_transformation.py
@@ -0,0 +1,8 @@
+from pyspark.sql.functions import col, to_date
+from config import BRONZE_PATH, SILVER_PATH
+from utils.helpers import ensure_dir
+
+
+def run_silver_job(spark):
+
+    return transformed
