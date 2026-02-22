faa85e91dc14ad2ea0eddf620f6d883056fafe3c
Md Shahedur Rahman <mr6139@nyu.edu>
2026-02-21 21:30:57 -0500

    Add HIGH_SURGE rule

--- jobs/surge_detection.py
+++ jobs/surge_detection.py
@@ -4,4 +4,14 @@ from utils.helpers import ensure_dir
 
 
 def run_surge_detection(spark):
+    print("Running Surge Detection...")
+
+    ensure_dir(SURGE_PATH)
+
+    df = spark.read.parquet(SILVER_PATH)
+
+    flagged = df.withColumn(
+        "surge_flag"
+    )
+
     return flagged
