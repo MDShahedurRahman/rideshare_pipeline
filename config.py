dbc805b476567c21bca924b218ce679ba3834bd4
Md Shahedur Rahman <mr6139@nyu.edu>
2026-02-21 21:11:12 -0500

    configure paths

---
+++ config.py
@@ -0,0 +1,7 @@
+SOURCE_FILE = "data/trips.csv"
+
+BRONZE_PATH = "output/bronze/"
+SILVER_PATH = "output/silver/"
+SURGE_PATH = "output/silver/surge_flagged/"
+GOLD_PATH = "output/gold/"
+REPORT_PATH = "output/reports/"
