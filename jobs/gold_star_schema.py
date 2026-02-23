050055381a2e385bb504e2d58d7aed5f07a15ae0
Md Shahedur Rahman <mr6139@nyu.edu>
2026-02-22 23:41:12 -0500

    Build fact_trips

--- jobs/gold_star_schema.py
+++ jobs/gold_star_schema.py
@@ -16,3 +16,16 @@ def run_gold_job(spark):
     dim_rider = df.select(
         "rider_id"
     ).dropDuplicates()
+
+    fact_trips = df.select(
+        "trip_id",
+        "driver_id",
+        "rider_id",
+        "distance_km",
+        "duration_min",
+        "fare_amount",
+        "avg_speed_kmh",
+        "trip_date",
+        "payment_method",
+        "surge_flag"
+    )
