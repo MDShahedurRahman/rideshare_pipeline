9e5191762ff3edf269ec278a89bf43a1da8b3ed3
Md Shahedur Rahman <mr6139@nyu.edu>
2026-02-22 23:59:14 -0500

    dd revenue by payment query

--- jobs/business_kpis.py
+++ jobs/business_kpis.py
@@ -4,3 +4,14 @@ from utils.helpers import ensure_dir
 
 
 def run_kpi_job(spark):
+    print("Generating Business KPIs...")
+
+    ensure_dir(REPORT_PATH)
+
+    fact = spark.read.parquet(GOLD_PATH + "fact_trips/")
+
+    revenue_by_city = (
+        fact.groupBy("payment_method")
+        .agg(sum("fare_amount").alias("total_revenue"))
+        .orderBy(desc("total_revenue"))
+    )
